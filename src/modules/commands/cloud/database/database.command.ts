64851a8b94d263edda3c200dc5cbe01653ddc879
starci183 <cuongnvtse160875@gmail.com>
2026-01-08 16:32:45 +0700

    Add SeedCommand to DatabaseCommand and DatabaseModule
    - Updated `DatabaseCommand` to include `SeedCommand` as a subcommand, enhancing database management capabilities.
    - Modified `DatabaseModule` to register `SeedCommand`, ensuring it is available for use in the application.

--- src/modules/commands/cloud/database/database.command.ts
+++ src/modules/commands/cloud/database/database.command.ts
@@ -1,12 +1,12 @@
 
 import { Command, CommandRunner } from "nest-commander"
-import { BackupCommand, RestoreCommand } from "./subs"
+import { BackupCommand, RestoreCommand, SeedCommand } from "./subs"
 import { Logger } from "@nestjs/common"
 
 @Command({
     name: "db",
     description: "manage db actions",
-    subCommands: [ BackupCommand, RestoreCommand ]
+    subCommands: [ BackupCommand, RestoreCommand, SeedCommand ]
 })
 export class DatabaseCommand extends CommandRunner {
     private readonly logger = new Logger(DatabaseCommand.name)
--- src/modules/commands/cloud/database/database.module.ts
+++ src/modules/commands/cloud/database/database.module.ts
@@ -3,13 +3,14 @@ import { Module } from "@nestjs/common"
 import {
     ConfigurableModuleClass,
 } from "./database.module-definition"
-import { BackupCommand, RestoreCommand } from "./subs"
+import { BackupCommand, RestoreCommand, SeedCommand } from "./subs"
 import { DatabaseCommand } from "./database.command"
 @Module({
     providers: [
         DatabaseCommand,
         BackupCommand,
         RestoreCommand,
+        SeedCommand,
     ],
 })
 export class DatabaseModule extends ConfigurableModuleClass {}
