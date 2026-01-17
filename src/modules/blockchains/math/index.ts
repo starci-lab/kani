1ce73b154fbba3007f89a468ec8eaff0340b4b4f
starci183 <cuongnvtse160875@gmail.com>
2026-01-17 13:28:38 +0700

    Remove zap.service export from math module to streamline service exports and enhance code clarity.

--- src/modules/blockchains/math/index.ts
+++ src/modules/blockchains/math/index.ts
@@ -1,6 +1,5 @@
 export * from "./math.module"
 export * from "./tick.service"
-export * from "./zap.service"
 export * from "./pool.service"
 export * from "./ensure.service"
 export * from "./fee.service"
