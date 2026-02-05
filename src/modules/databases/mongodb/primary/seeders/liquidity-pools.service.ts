44e623f38213dc7301dd1529171a2dc4a1bb4e46
starci183 <cuongnvtse160875@gmail.com>
2026-02-06 01:13:53 +0700

    Update liquidity pool fee type and add i32Type metadata
    - Changed the feeType in the liquidity pool seeder to a new value for better accuracy in fee representation.
    - Added i32Type metadata to enhance the schema definition for liquidity pools.

--- src/modules/databases/mongodb/primary/seeders/liquidity-pools.service.ts
+++ src/modules/databases/mongodb/primary/seeders/liquidity-pools.service.ts
@@ -147,9 +147,10 @@ export const data: Array<DeepPartial<LiquidityPoolSchema>> = [
         staleMs: ms("20s"),
         metadata: {
             packageId: "0xa5a0c25c79e428eba04fb98b3fb2a34db45ab26d4c8faf0d7e39d66a63891e64",
-            feeType: "0x91bfbc386a41afcfd9b2533058d7e915a1d3829089cc268ff4333d54d6339ca1::fee500bps::FEE500BPS",
+            feeType: "0x38a236095cb5a4fec3bdf42cf3e9c80f7ef68f1a835290a6cd107f156f8747f4::fee1500bps::FEE1500BPS",
             versionObject: "0xf1cf0e81048df168ebeb1b8030fad24b3e0b53ae827c25053fff0779c1445b6f",
             positionsObject: "0xf5762ae5ae19a2016bb233c72d9a4b2cba5a302237a82724af66292ae43ae52d",
+            i32Type: "0x91bfbc386a41afcfd9b2533058d7e915a1d3829089cc268ff4333d54d6339ca1::i32::I32",
         },
     },
     {
