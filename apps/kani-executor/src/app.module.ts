8c32b1919586fa9101ef34f1720566d1c08c4b9e
starci183 <cuongnvtse160875@gmail.com>
2026-02-18 00:22:10 +0700

    Comment out MongodbPrimary dependency in app module for cleaner configuration management

--- apps/kani-executor/src/app.module.ts
+++ apps/kani-executor/src/app.module.ts
@@ -275,7 +275,7 @@ import {
         TerminusModule.register({
             isGlobal: true,
             dependencies: [
-                DependencyName.MongodbPrimary,
+                //DependencyName.MongodbPrimary,
                 DependencyName.CacheRedis,
                 //DependencyName.ThrottlerRedis,
                 //DependencyName.Kafka,
